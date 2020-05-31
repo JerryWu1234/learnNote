5cf0033c0a17820c4afde02490fbfaaeb026e5f1
wuls <linsheng.wu@beantechs.com>
2020-05-31 14:09:58 +0800

    add

--- node/parser.js
+++ node/parser.js
@@ -2,27 +2,33 @@
  * @deprecated 遍历循环 用有限状态机找出所有的标签和属性
  *
  */
-let rules = []
 const css = require('css')
-
-const EOF = Symbol('EOF')
-let currentToken = ''
+const layout = require('./layout')
+const render = require('./render')
+let currentToken = null
 let currentAttribute = null
+
+let stack = [{ type: 'document', children: [] }]
 let currentTextNode = null
 
-let stack = [{ type: 'docment', children: [] }]
+let rules = []
+function addCSSRules(text) {
+  var ast = css.parse(text)
+  rules.push(...ast.stylesheet.rules)
+}
 
 function match(element, selector) {
   if (!selector || !element.attributes) {
     return false
   }
+
   if (selector.charAt(0) === '#') {
-    let attr = element.attributes.filter(attr.name === 'id')[0]
+    var attr = element.attributes.filter((attr) => attr.name === 'id')[0]
     if (attr && attr.value === selector.replace('#', '')) {
       return true
     }
   } else if (selector.charAt(0) === '.') {
-    let attr = element.attributes.filter(attr.name === 'class')[0]
+    var attr = element.attributes.filter((attr) => attr.name === 'class')[0]
     if (attr && attr.value === selector.replace('.', '')) {
       return true
     }
@@ -34,76 +40,10 @@ function match(element, selector) {
   return false
 }
 
-// 解析出的css规则树，从内往外遍历，匹配对应到元素上
-function computeCSS(element) {
-  // 查找当前元素的匹配的所有css属性，通过从里到外匹配先找到子层
-  let elements = stack.slice().reverse()
-  if (!element.computeStyle) {
-    element.computeStyle = {}
-  }
-  for (let rule of rules) {
-    let selectorParts = rule.selector[0].spilit(' ').reverse()
-    if (!match(element, selectorParts[0])) {
-      continue
-    }
-    let matched = false
-    let j = 1
-    for (let i = 0; i < elements.length; i++) {
-      if (match(elements[i], selectorParts[j])) {
-        j++
-      }
-    }
-    if (j >= selectorParts.length) {
-      matched = true
-    }
-    if (matched) {
-      let sp = specificity(rule.selector[0])
-      // 匹配到就会形成computedStyle
-      let computeStyle = element.computeStyle
-      for (let declaration of rule.declarations) {
-        if (!computeStyle[declaration.property]) {
-          computeStyle[declaration.property] = {}
-        }
-        if (!computeStyle[declaration.property].specificity) {
-          computeStyle[declaration.property].value = declaration.value
-          computeStyle[declaration.property].specificity = sp
-        } else if (
-          (compare(computeStyle[declaration.property].specificity), sp) < 0
-        ) {
-          computeStyle[declaration.property].value = declaration.value
-          computeStyle[declaration.property].specificity = sp
-          // for (let k = 0; k < 4; k++) {
-          //   computeStyle[declaration.property][declaration.value][k] += sp
-          // }
-        }
-      }
-      // for (let declaration of rule.declarations) {
-      //   if (!computeStyle[declaration.property]) {
-      //     computeStyle[declaration.property] = {}
-      //   }
-      //   computeStyle[declaration.property].value = declaration.value
-      // }
-    }
-  }
-}
-// [0,0,0,0] 计算权重，从右往左比较大小。
-function compare(sp1, sp2) {
-  if (sp1[0] - sp2[0]) {
-    return sp1[0] - sp2[0]
-  }
-  if (sp[1] - sp2[1]) {
-    return sp1[1] - sp2[1]
-  }
-  if (sp[2] - sp2[2]) {
-    return sp1[2] - sp2[2]
-  }
-  return sp1[3] - sp2[3]
-}
-// [0,0,0,0] 计算权重，当id时1的位置加一,当class时2的位置加1,内敛样式0加一
 function specificity(selector) {
-  let p = [0, 0, 0, 0]
-  let selectorParts = selector.spilit(' ')
-  for (let part of selectorParts) {
+  var p = [0, 0, 0, 0]
+  var selectorParts = selector.split(' ')
+  for (var part of selectorParts) {
     if (part.charAt(0) === '#') {
       p[1] += 1
     } else if (part.charAt(0) === '.') {
@@ -111,20 +51,73 @@ function specificity(selector) {
     } else {
       p[3] += 1
     }
-    return p
+  }
+  return p
+}
+
+function compare(sp1, sp2) {
+  if (sp1[0] - sp2[0]) {
+    return sp1[0] - sp2[0]
+  }
+  if (sp1[1] - sp2[1]) {
+    return sp1[1] - sp2[1]
+  }
+  if (sp1[2] - sp2[2]) {
+    return sp1[2] - sp2[2]
+  }
+  return sp1[3] - sp2[3]
+}
+
+function computeCSS(element) {
+  var elements = stack.slice().reverse() // 命中对应的CSS节点
+  if (!element.computedStyle) {
+    element.computedStyle = {}
+  }
+
+  for (let rule of rules) {
+    var selectorParts = rule.selectors[0].split(' ').reverse()
+    if (!match(element, selectorParts[0])) {
+      continue
+    }
+
+    let matched = false
+
+    var j = 1
+    for (var i = 0; i < elements.length; i++) {
+      if (match(elements[i], selectorParts[j])) {
+        j++
+      }
+    }
+    if (j >= selectorParts.length) {
+      matched = true
+    }
+
+    if (matched) {
+      var sp = specificity(rule.selectors[0])
+      var computedStyle = element.computedStyle
+      for (var declaration of rule.declarations) {
+        if (!computedStyle[declaration.property]) {
+          computedStyle[declaration.property] = {}
+        }
+
+        if (!computedStyle[declaration.property].specificity) {
+          computedStyle[declaration.property].value = declaration.value
+          computedStyle[declaration.property].specificity = sp
+        } else if (
+          compare(computedStyle[declaration.property].specificity, sp) < 0
+        ) {
+          for (var k = 0; k < 4; k++) {
+            computedStyle[declaration.property][declaration.value][k] += sp[k]
+          }
+        }
+      }
+    }
   }
 }
-// css解析解析出对应的css规则数rule
-function addCssRules(text) {
-  let ast = css.parse(text)
-  console.log(JSON.stringify(ast, null, '  '))
 
-  rules.push(...ast.stylesheet.rules)
-}
 function emit(token) {
   let top = stack[stack.length - 1]
-  // if (token.type != 'text') {
-  // }
+
   if (token.type === 'startTag') {
     let element = {
       type: 'element',
@@ -132,6 +125,7 @@ function emit(token) {
       attributes: [],
     }
     element.tagName = token.tagName
+
     for (let p in token) {
       if (p !== 'type' && p !== 'tagName') {
         element.attributes.push({
@@ -141,11 +135,10 @@ function emit(token) {
       }
     }
 
-    // 当创建一个猿猴需要立即计算css
-    computeCSS(element)
+    computeCSS(element) // TODO: 计算CSS的位置很重要，这里会触发重排，然后触发重绘
 
     top.children.push(element)
-    element.parent = top
+    // element.parent = top;
 
     if (!token.isSelfClosing) {
       stack.push(element)
@@ -154,14 +147,14 @@ function emit(token) {
     currentTextNode = null
   } else if (token.type === 'endTag') {
     if (top.tagName !== token.tagName) {
-      throw new Error("Tag start end doesn't match ")
+      throw new Error("Tag start end doesn't match!")
     } else {
       if (top.tagName === 'style') {
-        console.log('>>>>>>>', top.children[0].content)
-        addCssRules(top.children[0].content)
+        addCSSRules(top.children[0].content)
       }
       stack.pop()
     }
+    layout(top)
     currentTextNode = null
   } else if (token.type === 'text') {
     if (currentTextNode === null) {
@@ -171,61 +164,54 @@ function emit(token) {
       }
       top.children.push(currentTextNode)
     }
-
     currentTextNode.content += token.content
   }
 }
+
+const EOF = Symbol('EOF') // EOF: End Of File
+
 function data(c) {
-  if (c == '<') {
+  if (c === '<') {
     return tagOpen
   } else if (c === EOF) {
-    emit({ type: 'EOF' })
+    emit({
+      type: 'EOF',
+    })
     return
   } else {
-    emit({ type: 'text', content: c })
+    emit({
+      type: 'text',
+      content: c,
+    })
     return data
   }
 }
+
 function tagOpen(c) {
   if (c === '/') {
     return endTagOpen
-  } else if (c.match(/^[a-zA-Z]$/)) {
+  } else if (c.match(/^[a-zA-z]$/)) {
     currentToken = {
       type: 'startTag',
       tagName: '',
     }
     return tagName(c)
   } else {
+    emit({
+      type: 'text',
+      content: c,
+    })
     return
   }
 }
-function endTagOpen(c) {
-  if (c.match(/^[a-zA-Z]$/)) {
-    currentToken = {
-      type: 'endTag',
-      tagName: '',
-    }
-    return tagName(c)
-  } else if (c === '>') {
-  } else if (c === EOF) {
-  } else {
-  }
-}
-function selfClosingStartag(c) {
-  if (c === '>') {
-    currentToken.isSelfClosing = true
-    emit(currentToken)
-    return data
-  } else if (c === 'EDF') {
-  }
-}
+
 function tagName(c) {
   if (c.match(/^[\t\n\f ]$/)) {
     return beforeAttributeName
   } else if (c === '/') {
-    return selfClosingStartag
+    return selfClosingStartTag
   } else if (c.match(/^[a-zA-Z]$/)) {
-    currentToken.tagName += c
+    currentToken.tagName += c //.toLowerCase()
     return tagName
   } else if (c === '>') {
     emit(currentToken)
@@ -237,23 +223,24 @@ function tagName(c) {
 }
 
 function beforeAttributeName(c) {
-  if (c.match(/^[/t/n/f ]$/)) {
+  if (c.match(/^[\t\n\f ]$/)) {
     return beforeAttributeName
-  } else if (c === '>' || c === '/' || c === EOF) {
-    return afterAttributName(c)
+  } else if (c === '/' || c === '>' || c === EOF) {
+    return afterAttributeName(c)
   } else if (c === '=') {
+    // return beforeAttributeName;
   } else {
     currentAttribute = {
       name: '',
       value: '',
     }
-
     return attributeName(c)
   }
 }
+
 function attributeName(c) {
-  if (c.match(/^[\t\n\f ]$/) || c === '/' || c === '>' || c === EOF) {
-    return afterAttributName(c)
+  if (c.match(/^[\t\n\f]$/) || c === '/' || c === '>' || c === EOF) {
+    return afterAttributeName(c)
   } else if (c === '=') {
     return beforeAttributeValue
   } else if (c === '\u0000') {
@@ -263,48 +250,40 @@ function attributeName(c) {
     return attributeName
   }
 }
-function beforeAttributeValue(c) {
-  if (c.match(/^[/t/n/f ]$/) || c === '/' || c === '>' || c === EOF) {
+
+function afterAttributeName(c) {
+  if (c.match(/^[\t\n\f ]$/)) {
+    return afterAttributeName
+  } else if (c == '/') {
+    return selfClosingStartTag
+  } else if (c == '=') {
     return beforeAttributeValue
-  } else if (c === '"') {
-    return doubleQuotedAttributeValue
-  } else if (c === "'") {
-    return singleQuotedAttributeValue
-  } else if (c === '>') {
-  } else {
-    return UnquoteAttributeValue(c)
-  }
-}
-function afterAttributName(c) {
-  if (c.match(/^[/t/n/f ]$/) || c === '/' || c === '>' || c === EOF) {
-    return beforeAttributeValue
-  } else if (c === '"') {
-    return doubleQuotedAttributeValue
-  } else if (c === "'") {
-    return singleQuotedAttributeValue
-  } else if (c === '>') {
-  } else {
-    return UnquoteAttributeValue(c)
-  }
-}
-function UnquoteAttributeValue(c) {
-  if (c.match(/^[/t/n/f ]$/)) {
-    currentToken[currentAttribute.name] = currentAttribute.value
-    return beforeAttributeName
-  } else if (c === '/') {
-    currentToken[currentAttribute.name] = currentAttribute.value
-    return selfClosingStartag
-  } else if (c === '>') {
+  } else if (c == '>') {
     currentToken[currentAttribute.name] = currentAttribute.value
     emit(currentToken)
     return data
-  } else if (c === '\u0000') {
-  } else if (c === '"' || c === "'" || c === '<' || c === '=' || c === '`') {
-    return tagName
-  } else if (c === EOF) {
+  } else if (c == EOF) {
   } else {
-    currentAttribute.value += c
-    return UnquoteAttributeValue
+    currentToken[currentAttribute.name] = currentAttribute.value
+    currentAttribute = {
+      name: '',
+      value: '',
+    }
+  }
+  return attributeName(c)
+}
+
+function beforeAttributeValue(c) {
+  if (c.match(/^[\t\n\f ]$/) || c === '//' || c === '>' || c === EOF) {
+    return beforeAttributeValue
+  } else if (c === '"') {
+    return doubleQuotedAttributeValue
+  } else if (c === "'") {
+    return singleQuotedAttributeValue
+  } else if (c === '>') {
+    // return data;
+  } else {
+    return UnquotedAttributeValue(c)
   }
 }
 
@@ -319,6 +298,7 @@ function doubleQuotedAttributeValue(c) {
     return doubleQuotedAttributeValue
   }
 }
+
 function singleQuotedAttributeValue(c) {
   if (c === "'") {
     currentToken[currentAttribute.name] = currentAttribute.value
@@ -330,11 +310,12 @@ function singleQuotedAttributeValue(c) {
     return doubleQuotedAttributeValue
   }
 }
+
 function afterQuotedAttributeValue(c) {
-  if (c.match(/^[/t/n/f ]$/)) {
+  if (c.match(/^[\t\n\f ]$/)) {
     return beforeAttributeName
   } else if (c === '/') {
-    return selfClosingStartag
+    return selfClosingStartTag
   } else if (c === '>') {
     currentToken[currentAttribute.name] = currentAttribute.value
     emit(currentToken)
@@ -346,37 +327,110 @@ function afterQuotedAttributeValue(c) {
   }
 }
 
+function UnquotedAttributeValue(c) {
+  if (c.match(/^[\t\n\f ]$/)) {
+    currentToken[currentAttribute.name] = currentAttribute.value
+    return beforeAttributeName
+  } else if (c === '/') {
+    currentToken[currentAttribute.name] = currentAttribute.value
+    return selfClosingStartTag
+  } else if (c === '>') {
+    currentToken[currentAttribute.name] = currentAttribute.value
+    emit(currentToken)
+    return data
+  } else if (c === '\u0000') {
+  } else if (c === '"' || c === "'" || c === '<' || c === '=' || c === '`') {
+  } else if (c === EOF) {
+  } else {
+    currentAttribute.value += c
+    return UnquotedAttributeValue
+  }
+}
+
+function selfClosingStartTag(c) {
+  if (c === '>') {
+    currentToken.isSelfClosing = true
+    emit(currentToken)
+    return data
+  } else if (c === EOF) {
+  } else {
+  }
+}
+
+function endTagOpen(c) {
+  if (c.match(/^[a-zA-z]$/)) {
+    currentToken = {
+      type: 'endTag',
+      tagName: '',
+    }
+    return tagName(c)
+  } else if (c === '>') {
+  } else if (c === EOF) {
+  } else {
+  }
+}
+
 function parserHTMl(html) {
   let state = data
-
-  for (let [c, index] of html) {
+  for (let c of html) {
     state = state(c)
   }
   state = state(EOF)
-  //返回json数据，attribute 属性一样会包含属性样式并且包含specificity权重数组
+
+  console.log(JSON.stringify(stack[0], null, '  '))
+  debugger
   return stack[0]
 }
 
-parserHTMl(`<html maaa="a">
+let dom = parserHTMl(`<html maaa=a >
 <head>
-    <title>HTML parser</title>
-    <style>
-    body div #myid{
-    width:100px;
-    background-color: #ff5000;
+<style>
+.background {
+  display: flex;
+  width: 800px;
+  height: 600px;
+  background-color: rgb(255,255,255);
 }
-body div img{
-    width:30px;
-    background-color: #ff1111;
+.test {
+  display: flex;
+  width: 500px;
+  height: 400px;
+  background-color: rgb(253,226,30);
+  justify-content: center;
+  align-items: center;
 }
-    </style>
+.demo {
+  display: flex;
+  width: 200px;
+  height: 100px;
+  justify-content: center;
+  background-color: rgb(25,255,255);
+}
+.demo2 {
+  display: flex;
+  width: 100px;
+  height: 200px;
+  background-color: rgb(255,215,255);
+}
+</style>
 </head>
 <body>
-    <div>
-    34343
+  <div id="id" class="background">
+    <div class="test">
+      <div class="demo"></div>
+      <div class="demo2"></div>
     </div>
+    <img id="myid"/>
+    <img />
+  </div>
 </body>
-</html>`)
+</html>
+  
+`)
+
+let viewport = images(800, 600)
+render(viewport, dom)
+viewport.save('viewport.jpg')
 /**
  * @description css样式计算
  * 1.收集css规则
